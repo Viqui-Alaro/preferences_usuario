e59e080f61ede553e6c98c8ada989ed59cb1a7a0
Viqui Alaro <viquialaro241@gmail.com>
2022-03-10 23:59:19 -0400

    implementacion de radio list title

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -5,11 +5,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_preferences/widgets/side_menu.dart';
 
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
 
   static const String routerName = 'Settings';
 
   const SettingScreen ({Key? key}): super(key: key);
+
+  @override
+  _SettingScreenState createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+
+  bool isDarkmode = false;
+  int gender = 1;
+  String name= 'Maribel';
 
   @override
   Widget build(BuildContext context) {
@@ -27,26 +37,28 @@ class SettingScreen extends StatelessWidget {
               const Text('Ajustes',style: TextStyle(fontSize: 45,fontWeight: FontWeight.w300) ),
               const Divider(),
               SwitchListTile.adaptive(
-                value: true, 
+                value: isDarkmode, 
                 title: const Text('Darkmode'),
                 onChanged: (value){
-
+                    isDarkmode = value;
+                    setState(() { });
               }),
               Divider(),
               RadioListTile(
                 value: 1,
-                groupValue: 1, 
+                groupValue: gender, 
                 title: const Text('Masculino'), 
                 onChanged: (value){
-
+                  gender = 1;
+                    setState(() { });
                 }),
                 Divider(),
               RadioListTile(
                 value: 2,
-                groupValue: 1, 
+                groupValue: gender, 
                 title: const Text('Femenino'), 
                 onChanged: (value){
-                  
+                  setState(() { });
                 }),
                 const Divider(),
                 Padding(
